6ef8976916b7a8388267df881a7ce7b56878d96c
Samuel Souza <souzasamuel623@gmail.com>
2021-03-24 17:36:47 -0300

    Update Sm02Variaveis.cs

--- Sm02Variaveis.cs
+++ Sm02Variaveis.cs
@@ -6,7 +6,11 @@ namespace teste
     {
         static void Main(String[] args)
         {
-            
+            //Tipos mais comuns
+            string car = "audi";
+            int idade = 20;
+            float nota = 9.5f;
+            bool vivo = true;
         }
     }
 }
