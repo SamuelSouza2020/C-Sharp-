b8ac4994b8506f4b30ab53015e74b5976e4dc60f
Samuel Souza <souzasamuel623@gmail.com>
2021-03-24 17:24:47 -0300

    Hello World

---
+++ Sm01HelloWorld.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            //Grava na tela
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+        }
+    }
+}
