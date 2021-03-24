5e1e8a7d8d2615da63c05e030680370d15f9c2ec
Samuel Souza <souzasamuel623@gmail.com>
2021-03-24 17:27:22 -0300

    Hello World

--- Sm01HelloWorld.cs
+++ Sm01HelloWorld.cs
@@ -11,6 +11,7 @@ namespace teste
 
             //Grava dados e pula linha
             Console.WriteLine("Hello world");
+            //Lê a próxima linha
             Console.ReadLine();
         }
     }
