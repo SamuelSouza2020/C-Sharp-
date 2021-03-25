6f77b76b0b0484f3bce4801161e20eca2ec7f8dd
Samuel Souza <souzasamuel623@gmail.com>
2021-03-25 16:23:07 -0300

    Update Sm03Operacao.cs

--- Sm03Operacao.cs
+++ Sm03Operacao.cs
@@ -7,6 +7,10 @@ namespace teste
         static void Main(String[] args)
         {
             
+            int x = 10;
+            int y = 20;
+
+            int resultado = x + y; 
         }
     }
 }
