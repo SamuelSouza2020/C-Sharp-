64229325d81361274712e8c6cbf87fee4215b871
Samuel Souza <souzasamuel623@gmail.com>
2021-03-25 20:08:47 -0300

    Create Sm04DeIncremento.cs

---
+++ Sm04DeIncremento.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
