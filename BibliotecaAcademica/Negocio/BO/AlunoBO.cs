697b6781e42748a49fc32ec8e47a8609360fd81a
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-03-16 17:13:30 -0300

    Estrutura feita

--- BibliotecaAcademica/Negocio/BO/AlunoBO.cs
+++ BibliotecaAcademica/Negocio/BO/AlunoBO.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Negocio.Model;//validar
+using Negocio.DAO; //mandar
 
 namespace Negocio.BO
 {
