35fcb7de16da69af4a18f7c2d606b514d16531a4
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-03-16 18:29:05 -0300

    B

---
+++ BibliotecaAcademica/Negocio/DAO/ConexaoBanco.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace Negocio.DAO
+{
+    public class ConexaoBanco
+    {
+        public static MySqlConnection Conectar()
+        {
+            string stringConexao = @"Server=localhost;database=connectcsharptomysql;User=root;";
+            MySqlConnection conexao = new MySqlConnection(stringConexao);
+            conexao.Open();
+            return conexao;
+        }
+    }
+}
