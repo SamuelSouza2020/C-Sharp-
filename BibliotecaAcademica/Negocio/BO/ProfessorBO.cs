a910c5523ce809625a9498fde29c1acda47a4ef7
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-03-16 17:05:09 -0300

    Método de verificação

---
+++ BibliotecaAcademica/Negocio/BO/ProfessorBO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.BO
+{
+    //Metodo para avaliar os objetos antes de passarem para o banco de dados(Camada DAO)
+    public class ProfessorBO
+    {
+    }
+}
