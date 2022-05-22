7638469688d9a3af6200265c3232ae7a71f0b93c
Qodeless <souzasamuel623@gmail.com>
2022-05-22 10:59:38 -0300

    Bubble Sort

---
+++ ReviewArray/ArrayStudy/ArrayStudy/OparationArrays/ArrayOrder.cs
@@ -0,0 +1,29 @@
+﻿namespace ArrayStudy.OparationArrays
+{
+    internal class ArrayOrder
+    {
+        public void BubbleSortOrder(ref int[] array)
+        {
+            int previousValue;
+            for(int i = 0; i < array.Length; i++)
+            {
+                for(int j = 0; j < array.Length - 1; j++)
+                {
+                    if(array[j] > array[j + 1])
+                    {
+                        previousValue = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = previousValue;
+                    }
+                }
+            }
+        }
+        public void ArrayPrint(int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
--- ReviewArray/ArrayStudy/ArrayStudy/Program.cs
+++ ReviewArray/ArrayStudy/ArrayStudy/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ArrayStudy.OparationArrays;
+
+namespace ArrayStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayOrder arrayOrder = new ArrayOrder();
+            int[] array = new int[9] { 9, 5, 8, 4, 3, 1, 7, 2, 6 };
+
+            Console.WriteLine("Origin Array");
+            arrayOrder.ArrayPrint(array);
+
+            arrayOrder.BubbleSortOrder(ref array);
+            Console.WriteLine("Sorted Array");
+            arrayOrder.ArrayPrint(array);
+        }
+    }
+}
